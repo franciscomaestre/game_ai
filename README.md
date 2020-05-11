f0ffeb3c5630a55e3df74bf7cda7e626c40f8b4b
FRANCISCO MAESTRE <francisco@maestretorreblanca.com>
2020-05-11 08:18:44 -0500

    v0 Game AI

---
+++ README.md
@@ -0,0 +1,30 @@
+
+## A instalar
+
+atari-py==0.2.6
+cloudpickle==1.3.0
+future==0.18.2
+gym==0.17.1
+gym-super-mario-bros==7.3.0
+nes-py==8.1.1
+numpy==1.18.4
+opencv-python==4.2.0.34
+Pillow==7.1.2
+protobuf==3.11.3
+pyglet==1.5.0
+scipy==1.4.1
+six==1.14.0
+tensorboardX==2.0
+torch==1.5.0
+tqdm==4.46.0
+
+brew install ffmpge
+
+## Resumen
+
+Este código está preparado para funcionar correctamente con el Super Mario. Aún así, en esta etapa también ejecuta juegos de atari como el Space Invaders.
+
+A futuro, se optimizará para ellos siguiendo el repo https://github.com/greydanus/baby-a3c
+
+Para construir este repo, me basé en el repo https://github.com/uvipen/Super-mario-bros-A3C-pytorch/blob/master/README.md. A partir de su trabajo generé esta versión optimizada y refactorizada
+ 
---
+++ src/processes/train.py
@@ -0,0 +1,138 @@
+"""
+@author: Francisco Maestre. Modified version of Viet Nguyen version
+"""
+
+import torch
+from src.environments import create_train_env
+from src.models.actor_critic import ActorCritic
+import torch.nn.functional as F
+from torch.distributions import Categorical
+from collections import deque
+from tensorboardX import SummaryWriter
+import timeit
+
+
+def local_train(index, opt, global_model, optimizer, save=False):
+    #Usamos una semilla distinta en cada iteración para que no todos los caminos sean iguales
+    torch.manual_seed(123 + index)
+
+    if save:
+        start_time = timeit.default_timer()
+    writer = SummaryWriter(opt.log_path)
+
+    #Creamos el ambiente del juego
+    env, num_states, num_actions = create_train_env(opt=opt,video=False)
+    
+    ## Creamos la red que vamos a entrenar y la configuramos
+    local_model = ActorCritic(num_states, num_actions)
+    if opt.use_gpu:
+        local_model.cuda()
+    local_model.train()
+    state = torch.from_numpy(env.reset())
+    if opt.use_gpu:
+        state = state.cuda()
+    
+
+    ## REVISAR TIEMPOS CON Y SIN VIDEO
+    
+    done = True
+    curr_step = 0
+    curr_episode = 0
+
+    while True:
+        #Si hemos configurado que se guarde, cada X épocas se encargará de salvarlo. Esto actualizará el que vemos visualmente
+        if save:
+            if curr_episode % opt.save_interval == 0 and curr_episode > 0:
+                torch.save(global_model.state_dict(),
+                           "{}/a3c_{}_{}_{}_{}".format(opt.saved_path, opt.game, opt.world, opt.stage, opt.action_type))
+        
+            print("Process {}. Episode {}".format(index, curr_episode))
+        curr_episode += 1
+        local_model.load_state_dict(global_model.state_dict())
+        if done:
+            h_0 = torch.zeros((1, 512), dtype=torch.float)
+            c_0 = torch.zeros((1, 512), dtype=torch.float)
+        else:
+            h_0 = h_0.detach()
+            c_0 = c_0.detach()
+        if opt.use_gpu:
+            h_0 = h_0.cuda()
+            c_0 = c_0.cuda()
+
+        log_policies = []
+        values = []
+        rewards = []
+        entropies = []
+
+        for _ in range(opt.num_local_steps):
+            curr_step += 1
+            logits, value, h_0, c_0 = local_model(state, h_0, c_0)
+            policy = F.softmax(logits, dim=1)
+            log_policy = F.log_softmax(logits, dim=1)
+            entropy = -(policy * log_policy).sum(1, keepdim=True)
+
+            m = Categorical(policy)
+            action = m.sample().item()
+
+            state, reward, done, _ = env.step(action)
+            state = torch.from_numpy(state)
+            if opt.use_gpu:
+                state = state.cuda()
+            if curr_step > opt.num_global_steps:
+                done = True
+
+            if done:
+                curr_step = 0
+                state = torch.from_numpy(env.reset())
+                if opt.use_gpu:
+                    state = state.cuda()
+
+            values.append(value)
+            log_policies.append(log_policy[0, action])
+            rewards.append(reward)
+            entropies.append(entropy)
+
+            if done:
+                break
+
+        R = torch.zeros((1, 1), dtype=torch.float)
+        if opt.use_gpu:
+            R = R.cuda()
+        if not done:
+            _, R, _, _ = local_model(state, h_0, c_0)
+
+        gae = torch.zeros((1, 1), dtype=torch.float)
+        if opt.use_gpu:
+            gae = gae.cuda()
+        actor_loss = 0
+        critic_loss = 0
+        entropy_loss = 0
+        next_value = R
+
+        for value, log_policy, reward, entropy in list(zip(values, log_policies, rewards, entropies))[::-1]:
+            gae = gae * opt.gamma * opt.tau
+            gae = gae + reward + opt.gamma * next_value.detach() - value.detach()
+            next_value = value
+            actor_loss = actor_loss + log_policy * gae
+            R = R * opt.gamma + reward
+            critic_loss = critic_loss + (R - value) ** 2 / 2
+            entropy_loss = entropy_loss + entropy
+
+        total_loss = -actor_loss + critic_loss - opt.beta * entropy_loss
+        writer.add_scalar("Train_{}/Loss".format(index), total_loss, curr_episode)
+        optimizer.zero_grad()
+        total_loss.backward()
+
+        for local_param, global_param in zip(local_model.parameters(), global_model.parameters()):
+            if global_param.grad is not None:
+                break
+            global_param._grad = local_param.grad
+
+        optimizer.step()
+
+        if curr_episode == int(opt.num_global_steps / opt.num_local_steps):
+            print("Training process {} terminated".format(index))
+            if save:
+                end_time = timeit.default_timer()
+                print('The code runs for %.2f s ' % (end_time - start_time))
+            return
