002717d2e4589e81d00c22c3624cc80301b54aa2
FRANCISCO MAESTRE <francisco@maestretorreblanca.com>
2020-05-25 17:12:39 -0500

    Fix crop

--- experiments/plot_sample.py
+++ experiments/plot_sample.py
@@ -11,21 +11,7 @@ import numpy as np
 import gym
 import gym_super_mario_bros
 
-def preprocess(observation):
-    observation = cv2.cvtColor(cv2.resize(observation, (84, 110)), cv2.COLOR_RGB2GRAY)
-    observation = observation[26:110,:]
-    #ret, observation = cv2.threshold(observation,1,255,cv2.THRESH_BINARY)
-    return np.reshape(observation,( 1, 84, 84))
-
-'''
-def process_frame_84(frame, frame_conf):
-    frame = frame[frame_conf["crop1"]:frame_conf["crop2"] + 160, :160]
-    frame = frame.mean(2)
-    frame = cv2.resize(frame, (84, frame_conf["dimension2"]))
-    frame = cv2.resize(frame, (84, 84))
-    frame = np.reshape(frame, [1, 84, 84])
-    return frame
-'''
+from params_manager import ParamsManager
 
 def process_frame_84(observation, frame_conf):
     observation = cv2.cvtColor(cv2.resize(observation, ( frame_conf['scale'] + frame_conf["crop_x"], frame_conf['scale'] + frame_conf["crop_y"])), cv2.COLOR_RGB2GRAY)
@@ -35,20 +21,18 @@ def process_frame_84(observation, frame_conf):
         ret, observation = cv2.threshold(observation,1,255,cv2.THRESH_BINARY)
     return np.reshape(observation,( 1, 84, 84))
 
-env = gym.make("SpaceInvaders-v0")
-#env = gym.make("SuperMarioBros-1-1-v0")
+#env = gym.make("SpaceInvaders-v0")
+env = gym.make("SuperMarioBros-1-1-v0")
 
 env.reset()
 
-frame_conf = {}
+params_manager = ParamsManager.getInstance()
+env_params = params_manager.get_env_params('super_mario')
+env_params['useful_region'] = env_params['useful_region']['Default'] 
 
-frame_conf["scale"] = 84
-frame_conf["crop_y"] = 14
-frame_conf["crop_x"] = 0
-frame_conf["binary"] = True
 
 frame, reward0, terminal, info = env.step(0)
-observation0 = process_frame_84(frame, frame_conf)
+observation0 = process_frame_84(frame, env_params['useful_region'] )
 print("After processing: " + str(np.array(observation0).shape))
 
 plt.imshow(np.array(np.squeeze(frame)))
@@ -56,3 +40,4 @@ plt.show()
 
 plt.imshow(np.array(np.squeeze(observation0)))
 plt.show()
+
--- games/__init__.py
+++
@@ -1,9 +0,0 @@
-
-
-def make_train_env(env_name, env_conf):
-    if env_conf["type"] == "super_mario":
-        from games.super_mario import make_train_env as _make_train_env
-    if env_conf["type"] == "atari":
-        from games.atari import make_train_env as _make_train_env            
-    
-    return _make_train_env(env_name = env_name, env_conf = env_conf)
